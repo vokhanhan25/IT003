02f93e493c42ea2264bdecab46278dd86866b1e6
An Vo <vokhanhan25@gmail.com>
2020-04-28 16:02:33 +0700

    tc-6/stack

--- tc-06/stack-arr.cpp
+++ tc-06/stack-arr.cpp
@@ -42,16 +42,63 @@ bool Pop (STACK &s, int &x) {
     return false;
 }
 
+
 void OutputStack(STACK s) {
     cout << "STACK: ";
     for (int i = 0; i <= s.top; i++)
         cout << s.a[i] << " ";
+    cout << "\n";
+}
+
+void init()
+{
+    cout << "1 - Push" << endl;
+    cout << "2 - Pop" << endl;
+    cout << "3 - Top" << endl;
+    cout << "4 - Checking Empty" << endl;
+    cout << "5 - Checking Full" << endl;
+    cout << "-------------------" << endl;
+    cout << endl;
 }
 
 int main () {
     STACK s;
     CreateStack(s);
-    Push(s, 5);
-    Push(s, 3);
-    OutputStack(s);
+    int res, x;
+    while (true)
+    {
+        init ();
+        cin >> res;
+        switch (res)
+        {
+            case 1:
+                cin >> x;
+                Push(s, x);
+                cout << "\n";
+                break;
+            case 2:
+                Pop(s, x);
+                cout << "\n";
+                break;
+            case 3:
+                cout << "\n";
+                break;
+            case 4:
+                if (IsEmpty(s))
+                    cout <<"Stack rong\n\n";
+                else cout <<"Stack khong rong\n\n";
+                break;
+            case 5:
+                if (IsFull(s))
+                    cout << "Stack day\n\n";
+                else cout <<"Stack khong day\n\n";
+                break;
+            case 6:
+                cout <<"EXIT" << endl;
+                return 0;
+            default:
+                cout << "Ban da nhap sai, vui long nhap lai: \n\n";
+        }
+        OutputStack(s);
+    }
 }
