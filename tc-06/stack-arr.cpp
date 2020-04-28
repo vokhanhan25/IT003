377ad9d4dfbae615c6af4ee6a352ad6175465418
An Vo <vokhanhan25@gmail.com>
2020-04-28 15:47:58 +0700

    ADD tc-0/stack-arr

--- tc-06/stack-arr.cpp
+++ tc-06/stack-arr.cpp
@@ -3,45 +3,55 @@
 using namespace std;
 
 struct STACK {
-    long a[MAXN];
-    long n;
+    int a[MAXN];
+    int top;
 };
 
 
 void CreateStack(STACK &s) {
-    s.n = -1;
+    s.top = -1;
 }
 
 bool IsEmpty (STACK s) {
-    if (s.n == -1)
+    if (s.top == -1)
         return true;
     return false;
 }
 
 bool IsFull (STACK s) {
-    if (s.n >= MAXN)
+    if (s.top >= MAXN)
         return true;
     return false;
 }
 
-bool Push (STACK &s, long x) {
+bool Push (STACK &s, int x) {
     if (!IsFull(s)) {
-        s.n ++;
-        s.a[s.n] = x;
+        s.top ++;
+        s.a[s.top] = x;
         return true;
     }
     return false;
 }
 
-bool Pop (STACK &s, long &x) {
+bool Pop (STACK &s, int &x) {
     if (!IsEmpty(s)) {
-        x = s.a[s.n];
-        s.n --;
+        x = s.a[s.top];
+        s.top --;
         return true;
     }
     return false;
+}
+
+void OutputStack(STACK s) {
+    cout << "STACK: ";
+    for (int i = 0; i <= s.top; i++)
+        cout << s.a[i] << " ";
 }
 
 int main () {
-   
+    STACK s;
+    CreateStack(s);
+    Push(s, 5);
+    Push(s, 3);
+    OutputStack(s);
 }
