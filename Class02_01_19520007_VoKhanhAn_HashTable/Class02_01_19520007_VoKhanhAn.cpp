34a5e91ef476de8854300b57aa00fedde37a5315
An Vo <vokhanhan25@gmail.com>
2020-06-11 16:37:04 +0700

    ADD HashTable

--- Class02_01_19520007_VoKhanhAn_HashTable/Class02_01_19520007_VoKhanhAn.cpp
+++ Class02_01_19520007_VoKhanhAn_HashTable/Class02_01_19520007_VoKhanhAn.cpp
@@ -111,22 +111,137 @@ bool IsEmptyTable(LIST bucket[]) {
     return true;
 }
 
+bool DeleteAfterQ (LIST &l, NODE *Q, long &x)
+{
+    NODE *p;
+    if (Q != NULL) {
+        NODE *p = Q -> pNext;
+        Q -> pNext = p -> pNext;
+        x = p -> info;
+        if (p == l.pTail)
+            l.pTail = Q;
+        delete p;
+        return true;
+    }
+    return false;
+}
+
+bool DeleteHead(LIST &l, long &x)
+{
+    NODE *p;
+    if (l.pHead != NULL)
+    {
+        x = l.pHead -> info;
+        p = l.pHead;
+        l.pHead = l.pHead -> pNext;
+        if (l.pHead == NULL)
+            l.pTail = NULL;
+        delete p;
+        return true;
+    }
+    return false;
+}
+
+
+bool DeleteX(LIST &l, long x)
+{
+    NODE *Q = NULL, *p = l.pHead;
+    while (p != NULL && p->info != x)
+    {
+        Q = p;
+        p = p -> pNext;
+    }
+
+    if (p == NULL) return false;
+    if (Q != NULL) DeleteAfterQ(l, Q, x);
+    else DeleteHead(l, x);
+
+    return true;
+}
+
+void InputManual(LIST bucket[]) {
+    int value, req;
+    do {
+        cout << "Nhap gia tri can chen: ";
+        cin >> value;
+        Insert(bucket, value);
+        cout << "Ban co muon tiep tuc chen khong? (1/0): ";
+        cin >> req;
+    } while (req == 1);
+}
+
+void DeleteHashTable(LIST bucket[]) {
+    int key;
+    cout << "Nhap gia tri can xoa trong bang bam: ";
+    cin >> key;
+
+    int tableIndex = HashFunction(key);
+    if (DeleteX(bucket[tableIndex], key))
+        cout << "Da xoa thanh cong!\n";
+    else
+        cout << "Gia tri nhap vao khong co trong Hash Table!\n";
+}
+
+void Init() {
+    cout << "1: Insert Random\n";
+    cout << "2: Insert Manual\n";
+    cout << "3: Output\n";
+    cout << "4: Delete\n";
+    cout << "5: Search Node\n";
+    cout << "6: Hash Table Is Empty?\n";
+    cout << "7: Thoat khoi chuong trinh!\n";
+    cout << "\n";
+}
+
 int main() {
     LIST bucket [M];
     InitBucket(bucket);
-    Insert(bucket, 76);
-    Insert(bucket, 93);
-    Insert(bucket, 40);
-    Insert(bucket, 47);
-    Insert(bucket, 10);
-    Insert(bucket, 55);
+    int option, key;
 
-    OutputHashTable(bucket);
-    cout << SearchNode(bucket, 75) << "\n";
-    IsEmptyBucket(bucket);
-
-    if (IsEmptyTable(bucket))
-        cout << "Hash Table is empty\n";
-    else 
-        cout << "Hash Table NOT is empty\n";
+    while (true) {
+        Init();
+        cout << "Nhap lua chon: ";
+        cin >> option;
+        switch(option) {
+            case 1:
+                break;
+            case 2:
+                InputManual(bucket);
+                break;
+            case 3:
+                OutputHashTable(bucket);
+                break;
+            case 4:
+                DeleteHashTable(bucket);
+                break;
+            case 5:
+                cout << "Nhap gia tri can tim kiem: ";
+                cin >> key;
+                if (SearchNode(bucket, key) != NULL) 
+                    cout << "Dia chi Node can tim kiem: " << SearchNode(bucket, key) << "\n";
+                else 
+                    cout << "Gia tri can tim kiem KHONG co trong Hash Table\n";
+                break;
+            case 6:
+                 IsEmptyBucket(bucket);
+                if (IsEmptyTable(bucket))
+                    cout << "Hash Table is empty\n";
+                else 
+                    cout << "Hash Table NOT is empty\n";
+                break;
+            case 7:
+                cout << "EXIT!\n";
+                return 0;
+            default:
+                cout << "Khong hop le, vui long nhap lai!\n";
+        }
+    }
+    
+    // Insert(bucket, 76);
+    // Insert(bucket, 93);
+    // Insert(bucket, 40);
+    // Insert(bucket, 47);
+    // Insert(bucket, 10);
+    // Insert(bucket, 55);
+   
 }
